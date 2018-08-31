3c08420eca167483c2f9f9ec001e9c86551392d7
Yang Zhang <zhangya3@oregonstate.edu>
2018-08-30 17:16:35 -0700

    update function

--- R/yzmiscf_functions.R
+++ R/yzmiscf_functions.R
@@ -1,27 +1,25 @@
-#' @title A Dog Function
-#'
-#' @description This function allows you to express your love for the superior furry animal.
-#' @param agree Do you agree dogs are the best pet? Defaults to TRUE.
-#' @keywords dogs
+#' @title A Miscellaneous Function
+#' @description This function allows you to take PCR raw Ct values of technical replicates,
+#' de-multiplicates each sample, and returns with a unique average Ct value for each biological sample.
+#' @param raw_data ##raw Ct values from PCR, duplicates or triplicates
+#' @keywords data wrangling
 #' @export
 #' @examples
-#' dogs_over_cats()
+#' cleanme()
 
-dogs_over_cats <- function(agree=TRUE){
-  if(agree==TRUE){
-    print("Woof woof!!!")
-  }
-  else {
-    print("Try again.")
-  }
+
+cleanme <- function(raw_data, clean_output){
+  vars <- raw_data %>%
+    select(id, variable) %>%
+    distinct(id, .keep_all = TRUE)
+  clean_output <- raw_data %>%
+    group_by(id) %>%
+    summarise_if(is.numeric, mean, na.rm = TRUE) %>%
+    mutate(treatment = vars$variable) %>%
+    select(id, treatment, everything())
+  clean_output
 }
 
 
-newf <- function(agree=TRUE){
-  if(agree==TRUE){
-    print("Woof woof!!!")
-  }
-  else {
-    print("Try again.")
-  }
-}
+
+
